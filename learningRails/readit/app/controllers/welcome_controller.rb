c18d2646e5711ada025319495ee9845b6afad76d
anacanm <anacan@mangelsdorf.io>
2020-04-13 15:39:34 -0700

    Remove disp

--- learningRails/readit/app/controllers/welcome_controller.rb
+++ learningRails/readit/app/controllers/welcome_controller.rb
@@ -3,15 +3,4 @@ class WelcomeController < ApplicationController
     @i = 4
     render
   end
-
-  def disp
-    if (params.has_key?(:id) && params.has_key?(:random))
-      if(params[:id] == 25)
-        @random  = params[:random]
-      end
-    end
-  end
-
-  def root
-  end
 end
