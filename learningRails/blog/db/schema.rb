c9238de316ce9df9324d8f1cb499098f2d697a86
anacanm <anacan@mangelsdorf.io>
2020-04-14 10:18:11 -0700

    Update User and Article model

--- learningRails/blog/db/schema.rb
+++ learningRails/blog/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_163948) do
+ActiveRecord::Schema.define(version: 2020_04_14_170021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "post"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "password"
