b0ef6f6ab18e4cf17cfde2544356cf8510897b15
anacanm <anacan@mangelsdorf.io>
2020-04-28 21:18:27 -0700

    Add first Go webserver

---
+++ learningGo/web/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my first Go website!</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe("localhost:3000", nil)
+
+}
