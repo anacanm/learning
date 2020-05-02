df9a29254eca8a79a215cba7b35e913b4e2e1ad9
anacanm <anacan@mangelsdorf.io>
2020-05-02 14:32:02 -0700

    Playing around with custom types

--- learningGo/type/main.go
+++ learningGo/type/main.go
@@ -4,20 +4,34 @@ import (
 	"fmt"
 )
 
-func main() {
-	var a int = 42
-	fmt.Println(a)
-	fmt.Printf("%T\n", a)
+// Hour represents the hour :)
+type Hour int
 
-	type myType int
-
-	var b myType = 43
-	fmt.Println(b)
-	fmt.Printf("%T\n", b)
-
-	var c int = int(b)
-	fmt.Println(c)
-	fmt.Printf("%T\n", c)
-
+// PrintHour prints the Hour to stdout
+func PrintHour(h Hour) {
+	fmt.Printf("%#v hour", h)
 }
+
+func main() {
+	//	var a int = 42
+	//	fmt.Println(a)
+	//	fmt.Printf("%T\n", a)
+	//
+	//	type myType int
+	//
+	//	var b myType = 43
+	//	fmt.Println(b)
+	//	fmt.Printf("%T\n", b)
+	//
+	//	var c int = int(b)
+	//	fmt.Println(c)
+	//	fmt.Printf("%T\n", c)
+	var h Hour
+	h = 20
+	PrintHour(h)
+	var i int
+	i = 15
+	PrintHour(i)
+}
+
 // update main
