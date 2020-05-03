99ea786b9cb5bb10b5351f794450d9595d3979ab
anacanm <anacan@mangelsdorf.io>
2020-05-02 18:07:46 -0700

    Add documentation

--- learningGo/web/main.go
+++ learningGo/web/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// creates a function that handles the request passed to /
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Set Content Headers!</h1>")
